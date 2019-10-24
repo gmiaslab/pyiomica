472cb4c1fa79dd640f998c8aa78a6513ac43040c
Domanskyi <s.domanskyi@gmail.com>
2019-10-24 16:07:34 -0400

    version update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,9 +46,9 @@ copyright = '2019, S. Domanskyi, C. Piermarocchi, G. Mias'
 author = 'S. Domanskyi, C. Piermarocchi, G. Mias'
 
 # The short X.Y version
-version = '1.0.2'
+version = '1.1.0'
 # The full version, including alpha/beta/rc tags
-release = '1.0.2'
+release = '1.1.0'
 
 
 # -- General configuration ---------------------------------------------------
